4dabee87b0d676f6865d35077425aa5485f2d4a9
qige2016 <286882998@qq.com>
2020-02-09 21:52:05 +0800

    refactor: use errorHandler and illegalHandler in interceptors

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,13 +8,12 @@ import router from '@/router'
 
 const isSign = false // 开启关闭sign签名验证
 const securityKey = '' // 后端提供安全码
-const uuid = 'cms' + guid()
 
 // 错误提示信息
 const tipText = {
   400: {
-    89002: '登录无效，请重登',
-    89013: '登录超时，请重登'
+    '89002': '登录无效，请重登',
+    '89013': '登录超时，请重登'
   },
   404: '请求不存在',
   500: '服务器内部错误',
@@ -31,62 +30,93 @@ const showMessage = (
   Message({ type, showClose, duration, message })
 }
 
+/**
+  * 异常处理
+  * 自定义错误消息体 {
+    "errorCode": "string",
+    "errorMsg": "string"
+  }
+  * 自行修改errorCode和errorMsg
+*/
+const errorHandler = error => {
+  const { response } = error
+  if (response && response.status) {
+    const { status, data } = response
+    // 在tipText中有定义
+    if (tipText[status]) {
+      if (status === 400) {
+        // 未在tipText[400]中定义，使用默认tip
+        const msg = tipText[status][data.errorCode]
+        msg ? showMessage(msg) : showMessage(data.errorMsg)
+        // token失效时跳转
+        ;(data.errorCode === '89002' || data.errorCode === '89013') &&
+          store.dispatch('manager/fedLogOut').then(() => router.push('/login'))
+      } else {
+        // 404、500、502
+        showMessage(tipText[status])
+      }
+    } else {
+      // 未在tipText中定义，使用默认tip
+      showMessage(data.errorMsg)
+    }
+  }
+}
+
 // create an axios instance
 const service = axios.create({
   baseURL: '/',
   timeout: 30000, // request timeout
   headers: {
-    msgId: uuid,
+    msgId: 'cms' + guid(),
     from: 'PORTAL',
     'Content-Type': 'application/json;charset=UTF-8'
   }
 })
 
+/**
+ * 非法字符处理
+ */
+const illegalHandler = (data, config) => {
+  // 不包含 & 字符
+  if (!legitimacy(data)) {
+    // 包含 & 字符取消请求
+    showMessage('&为非法字符,请重新输入')
+    const CancelToken = axios.CancelToken
+    const source = CancelToken.source()
+    config.cancelToken = source.token
+    // 取消请求
+    source.cancel()
+  }
+}
+
 // request interceptor
 service.interceptors.request.use(
   config => {
-    // get 方法先判断是否包含 & 字符
-    if (config.method === 'get') {
-      // 不包含 & 字符
-      if (!legitimacy(config.params)) {
-        // 包含 & 字符取消请求
-        showMessage('&为非法字符,请重新输入')
-        const CancelToken = axios.CancelToken
-        const source = CancelToken.source()
-        config.cancelToken = source.token
-        // 取消请求
-        source.cancel()
-        return config
-      }
-    } else {
-      if (!legitimacy(config.data)) {
-        // 包含 & 字符取消请求
-        showMessage('&为非法字符,请重新输入')
-        const CancelToken = axios.CancelToken
-        const source = CancelToken.source()
-        config.cancelToken = source.token
-        // 取消请求
-        source.cancel()
-        return config
-      }
-      // 上传文件
-      if (isFormData(config.data)) {
-        config.headers['Content-Type'] = 'multipart/form-data'
-      }
-    }
     // token
     if (localStorage.getItem('LG_TK')) {
       config.headers['token'] = localStorage.getItem('LG_TK')
     }
-    // 签名
-    if (isSign && !isFormData(config.data)) {
-      if (config.method === 'get') {
-        let timestamp = new Date().getTime()
-        config.params = getSign(timestamp, config.params, securityKey)
-      } else {
-        let timestamp = new Date().getTime()
-        config.data = postSign(timestamp, config.data, securityKey)
-      }
+    //  在这里：可以根据业务需求可以在发送请求之前做些什么
+    // 非法字符处理
+    config.method === 'get'
+      ? illegalHandler(config.params, config)
+      : illegalHandler(config.data, config)
+    // 文件上传
+    if (isFormData(config.data)) {
+      config.headers['Content-Type'] = 'multipart/form-data'
+      // 签名
+    } else if (isSign) {
+      config.method === 'get'
+        ? (config.params = getSign(
+            new Date().getTime(),
+            config.params,
+            securityKey
+          ))
+        : (config.data = postSign(
+            new Date().getTime(),
+            config.data,
+            securityKey
+          ))
     }
     return config
   },
@@ -106,34 +136,8 @@ service.interceptors.response.use(
     /**
      * 通过 xmlhttprequest 来状态码标识error
      */
-    // console.log('error：' + error.response.data.errorMsg, error.response.status) // for debug
-    if (error && error.response) {
-      const data = error.response.data
-      const status = error.response.status + '' // number转为string
-      // 在tipText中有定义
-      if (tipText[status]) {
-        if (status === '400') {
-          // 未在tipText['400']中定义，使用默认tip
-          const msg = tipText['400'][data.errorCode]
-          msg ? showMessage(msg) : showMessage(data.errorMsg)
-          // 如果登录超时，前端退出后转跳登录页重新登录
-          data.errorCode === '89002' &&
-            store
-              .dispatch('manager/fedLogOut')
-              .then(() => router.push('/login'))
-          data.errorCode === '89013' &&
-            store
-              .dispatch('manager/fedLogOut')
-              .then(() => router.push('/login'))
-        } else {
-          // 404、500、502
-          showMessage(tipText[status])
-        }
-      } else {
-        // 未在tipText中定义，使用默认tip
-        showMessage(data.errorMsg)
-      }
-    }
+    // 异常处理
+    errorHandler(error)
     return Promise.reject(error)
   }
 )
